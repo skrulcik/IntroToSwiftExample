f7134599c2ba620ec7373855ff5d3bc60835ae67
Scott Krulcik <skrulcik@andrew.cmu.edu>
2016-01-23 17:47:52 -0500

    Added Item object to model

---
+++ ShoppingList/Item.swift
@@ -0,0 +1,20 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Scott Krulcik on 1/23/16.
+//  Copyright © 2016 Scott Krulcik. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var name: String
+    var count: Int
+
+    init(name: String, count: Int) {
+        self.name = name
+        self.count = count
+    }
+}
+
